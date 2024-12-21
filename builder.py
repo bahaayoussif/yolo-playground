7362aff4dc958d6d1e85026149e7a1f8c668d535
Hijazi's Kitchen <40371707+bahaayoussif@users.noreply.github.com>
2024-12-21 21:33:21 +0200

    builder.py

---
+++ builder.py
@@ -0,0 +1,41 @@
+from ultralytics import YOLO
+import cv2
+from PIL import Image
+
+
+class Builder:
+    def __init__(self, model_name):
+        models = {'yolo 3': 'models/yolov3.yaml',
+                  'yolo 5': 'models/yolov5.yaml',
+                  'yolo 6': 'models/yolov6.yaml',
+                  'yolo 8': 'models/yolov8.yaml',
+                  'yolo 9': 'models/yolov9m.yaml',
+                  'yolo 10': 'models/yolov10x.yaml', }
+        self.model_path = models[model_name]
+        self.model = YOLO(self.model_path)
+
+    def info(self):
+        return self.model.info()
+
+    def train(self, dataset_path, epochs):
+        self.model.train(data=dataset_path, epochs=epochs)
+
+    def validate(self, dataset_path=None):
+        if dataset_path is None:
+            self.model.val()
+        else:
+            self.model.val(data=dataset_path)
+
+    def predict(self, image_path, save_image=True, save_text=True, multiple=False):
+        if multiple:
+            images = [Image.open(i_path) for i_path in image_path]
+            results = self.model.predict(source=images, save_image=save_image, save_txt=save_text)
+            return results
+        else:
+            image = Image.open(image_path)
+            results = self.model.predict(source=image, save_image=save_image, save_txt=save_text)
+            return results
+
+
+model = Builder('yolo 10')
+model.info()
