308044f2f5a822a9e8228f4167040a6d7d1f5035
Hijazi's Kitchen <40371707+bahaayoussif@users.noreply.github.com>
2024-12-22 12:30:40 +0200

    write yolo class for creating yolo tasks very fast

--- yolo.py
+++ yolo.py
@@ -4,12 +4,18 @@ from config import YOLO_MODELS
 
 class Yolo:
     def __init__(self, version, task, size):
-        version = YOLO_MODELS[version]
-        task = version[task]
-        model_path = task[size]
+        self.version = version
+        model_path = YOLO_MODELS[self.version]
+        self.task = task
+        model_path = model_path[self.task]
+        self.size = size
+        model_path = model_path[self.size]
         self.model = YOLO(model_path)
 
     def get_info(self):
+        print(f'yolo version: {self.version}')
+        print(f'yolo task: {self.task}')
+        print(f'model size: {self.size}')
         print(self.model.info())
 
     def train(self):
@@ -32,5 +38,5 @@ class Yolo:
 
 
 # Instantiate the Yolo class
-yolo = Yolo(version=11, task='detect', size='nano')
+yolo = Yolo(version=11, task='detect', size='xlarge')
 yolo.get_info()
